59eac669fe6b007ea393a0b3b3ac96b6984d6183
Khwilo Kabaka <khwilowatai@gmail.com>
2020-11-14 12:10:40 +0300

    Set width to 80% for devices with large screens widths

--- container/Header.js
+++ container/Header.js
@@ -6,24 +6,26 @@ import styles from '../styles/Header.module.css';
 
 const Header = () => (
   <header className={styles.header}>
-    <div className={styles.logo}>
-      <Link href='/'>
-        <div className={styles.logoContainer}>
-          <Image
-            src='/assets/images/home-icon.svg'
-            alt='home icon'
-            width='18px'
-            height='18px'
-          />
-          <h1 className={styles.logoTitle}>HD</h1>
-        </div>
-      </Link>
-    </div>
-    <Nav />
-    <div>
-      <Link href='/login'>
-        <a className={styles.logIn}>Log in</a>
-      </Link>
+    <div className={styles.headerContainer}>
+      <div className={styles.logo}>
+        <Link href='/'>
+          <div className={styles.logoContainer}>
+            <Image
+              src='/assets/images/home-icon.svg'
+              alt='home icon'
+              width='18px'
+              height='18px'
+            />
+            <p className={styles.logoTitle}>HD</p>
+          </div>
+        </Link>
+      </div>
+      <Nav />
+      <div>
+        <Link href='/login'>
+          <a className={styles.logIn}>Log in</a>
+        </Link>
+      </div>
     </div>
   </header>
 );
